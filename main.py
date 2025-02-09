93dc93c5e63ee6e0769341779a16b4867fc66dc4
Faiq Khan <157122105+FakerPK@users.noreply.github.com>
2025-02-09 15:56:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -180,5 +180,4 @@ if __name__ == "__main__":
 # Adding this to make the language percentage equal for better searh results
 # Adding this to make the language percentage equal for better searh results
 # Adding this to make the language percentage equal for better searh results
-# Adding this to make the language percentage equal for better searh results
-# Adding this to make the language percentage equal for better searh results
+# Adding this to make the language percentage equal for better searh 
